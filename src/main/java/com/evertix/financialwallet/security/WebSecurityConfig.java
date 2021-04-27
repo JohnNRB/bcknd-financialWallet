7ec78c3f0579e7210547689a2d095efabd045d0f
MSAlbert40 <54085998+MSAlbert40@users.noreply.github.com>
2021-04-26 23:59:34 -0500

    Implementation Enterprise

--- src/main/java/com/evertix/financialwallet/security/WebSecurityConfig.java
+++ src/main/java/com/evertix/financialwallet/security/WebSecurityConfig.java
@@ -65,6 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/enterprise/**").permitAll()
                 .anyRequest().authenticated();
         httpSecurity.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
